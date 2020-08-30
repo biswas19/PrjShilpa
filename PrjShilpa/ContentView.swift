17f518899ca3437747509329f64543d3fd434bbd
Biswas Shilpa <biswas19@yahoo.com>
2020-08-29 21:39:51 -0500

    Initial Commit

---
+++ PrjShilpa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrjShilpa
+//
+//  Created by Pravangsu Biswas on 8/29/20.
+//  Copyright © 2020 Pravangsu Biswas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
